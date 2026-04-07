b00692bb49ed65c4d4d5ccbe22ba683059ba6f8c
mohamedshafeer111 <mohamedshafeersha@gmail.com>
2026-04-07 11:53:14 +0530

    Updated backend URL tunnel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,6 +14,6 @@
 // };
 export const environment = {
   production: true,
-  apiUrl: ' https://likewise-attention-jerusalem-desire.trycloudflare.com/api/',
+  apiUrl: ' https://worked-awareness-proposals-frequencies.trycloudflare.com/api/',
   wsUrl: 'wss://random-name.trycloudflare.com/ws/ZoneCount'
 };
