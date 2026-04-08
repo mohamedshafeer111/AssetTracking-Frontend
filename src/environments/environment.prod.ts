2ce4b643dd24cb4f5a2da13148c59d0b95c30c43
mohamedshafeer111 <mohamedshafeersha@gmail.com>
2026-04-08 17:57:49 +0530

    Updated backend URL tunnel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -14,6 +14,6 @@
 // };
 export const environment = {
   production: true,
-  apiUrl: 'https://barbara-limits-sitemap-attempted.trycloudflare.com/api/',
+  apiUrl: 'https://darwin-where-members-gamecube.trycloudflare.com/api/',
   wsUrl: 'wss://random-name.trycloudflare.com/ws/ZoneCount'
 };
